f748c914ace6855ca12a8db16ef052a67e4ac2f8
Karan Nehra <karanehra@gmail.com>
2019-12-14 23:38:56 +0530

    New implementation done. Drag and drop nodes and addtion

--- src/views/dashboard/components/trees/detailNew.js
+++ src/views/dashboard/components/trees/detailNew.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { callGetTreeByIdApi } from "../../../../utils/apis/apiService";
-import { TextField, Button } from "@material-ui/core/";
+import { TextField, Button, Typography } from "@material-ui/core/";
 import { Node, composeTreeFromObject } from "../../../../utils/treeHandler";
+import "@styles/views/tree.scss";
 
 class TreeDetailPage extends Component {
   state = {
@@ -10,7 +11,8 @@ class TreeDetailPage extends Component {
     addNodeTitle: "",
     /**@type {Node} */
     treeInstance: null,
-    activeNodeID: null
+    activeNodeID: "none",
+    nodes: []
   };
 
   handleChange = event => {
@@ -36,79 +38,68 @@ class TreeDetailPage extends Component {
   }
 
   handleAddNode = () => {
-    // const {
-    //   match: {
-    //     params: { id }
-    //   }
-    // } = this.props;
-    const { addNodeTitle, activeNodeID, treeInstance } = this.state;
+    const { addNodeTitle, nodes } = this.state;
     let node = new Node(addNodeTitle);
-    treeInstance.addNodeByID(node, activeNodeID);
-    this.setState({ treeInstance });
+    nodes.push(node);
+    this.setState({ nodes });
   };
 
   setActiveNode = activeNodeID => () => {
-    console.log(activeNodeID);
     this.setState({ activeNodeID });
-  };
-
-  makeDraggable = event => {
-    console.log(event);
   };
 
   dragStartX = "";
   dragStartY = "";
+  diffX = "";
+  diffY = "";
 
-  drag = event => {
+  drag = activeNodeID => event => {
     this.dragStartX = event.clientX;
     this.dragStartY = event.clientY;
     event.dataTransfer.setData("some", event.target.id);
+    this.setState({ activeNodeID });
   };
 
   drop = event => {
     event.persist();
     let data = event.dataTransfer.getData("some");
-    document.getElementById(data).style.top = event.clientY;
-    document.getElementById(data).style.left = event.clientX;
+    document.getElementById(data).style.top =
+      document.getElementById(data).offsetTop + this.diffY;
+    document.getElementById(data).style.left =
+      document.getElementById(data).offsetLeft + this.diffX;
   };
 
   allowDrop = event => {
     event.preventDefault();
-    console.log(
-      this.dragStartX - event.clientX,
-      this.dragStartY - event.clientY
-    );
-    console.log(event.target.id);
-    let diffX = event.clientX - this.dragStartX;
-    let diffY = event.clientY - this.dragStartY;
-    event.target.style.left = `${event.target.style.left + diffX}px`;
-    event.target.style.top = `${event.target.style.top + diffY}px`;
-    // console.log(
-    //   "allowdrop",
-    //   event.clientX,
-    //   event.clientY,
-    //   event.screenX,
-    //   event.screenY,
-    //   event.currentTarget
-    // );
-    // event.currentTarget.style.top = event.clientX;
-    // event.currentTarget.style.left = event.clientY;
+    this.diffX = event.clientX - this.dragStartX;
+    this.diffY = event.clientY - this.dragStartY;
   };
 
   render() {
-    const { addNodeTitle, treeInstance, activeNodeID } = this.state;
+    const { addNodeTitle, nodes, activeNodeID } = this.state;
     return (
       <React.Fragment>
-        <TextField
-          variant="outlined"
-          label={`Add node to ${activeNodeID || "_"}`}
-          value={addNodeTitle}
-          name="addNodeTitle"
-          onChange={this.handleChange}
-        />
-        <Button variant="outlined" onClick={this.handleAddNode}>
-          Add
-        </Button>
+        <div className="toolbar">
+          <Typography variant="h5" className="selected-node">
+            <b>{activeNodeID}</b>
+          </Typography>
+          <TextField
+            variant="outlined"
+            label={`Add node`}
+            value={addNodeTitle}
+            margin="dense"
+            name="addNodeTitle"
+            onChange={this.handleChange}
+          />
+          <Button
+            variant="contained"
+            size="large"
+            color="primary"
+            onClick={this.handleAddNode}
+          >
+            Add
+          </Button>
+        </div>
         <div
           className="drag-area"
           style={{
@@ -118,30 +109,19 @@ class TreeDetailPage extends Component {
           onDrop={this.drop}
           onDragOver={this.allowDrop}
         >
-          {treeInstance && (
-            <div
-              style={{ width: "300px", height: "300px", position: "absolute" }}
-              draggable
-              onDragStart={this.drag}
-              id="div1"
-            >
-              <svg width={300} height={300}>
-                <g onClick={this.setActiveNode(treeInstance.ID)}>
-                  <circle
-                    cx={150}
-                    cy={150}
-                    r={50}
-                    fill={"white"}
-                    stroke={activeNodeID === treeInstance.ID ? "blue" : "red"}
-                    strokeWidth="3"
-                  ></circle>
-                  <text textAnchor="middle" x={150} y={150}>
-                    {treeInstance.title}
-                  </text>
-                </g>
-              </svg>
-            </div>
-          )}
+          {nodes.length > 0 &&
+            nodes.map((node, i) => (
+              <div
+                key={i}
+                className={activeNodeID === node.ID ? "node active" : "node"}
+                draggable
+                onDragStart={this.drag(node.ID)}
+                id={`node-${node.ID}`}
+                onClick={this.setActiveNode(node.ID)}
+              >
+                {node.title}
+              </div>
+            ))}
         </div>
       </React.Fragment>
     );
