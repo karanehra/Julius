a3c725fa074ae50fbaa4526ade47a3fe977393b8
Karan Nehra <karanehra@gmail.com>
2019-12-31 20:01:03 +0530

    Added a querying functionality

--- src/views/dashboard/components/articles.js
+++ src/views/dashboard/components/articles.js
@@ -5,22 +5,37 @@ import {
   Typography,
   ExpansionPanelDetails,
   Button,
-  Dialog
+  Dialog,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  FormGroup,
+  TextField,
+  OutlinedInput,
+  InputAdornment
 } from "@material-ui/core";
 import { connect } from "react-redux";
 import { getArticleDataAsyncAction } from "@actions/articles.actions";
 import ExpandMore from "@material-ui/icons/ExpandMore";
 import GenericText from "@shared/genericText";
 import { callParseArticleApi } from "@utils/apis/apiService";
+import { Search } from "@material-ui/icons";
 import "@styles/views/articles.scss";
 
 class ArticlesPage extends Component {
   componentDidMount() {
-    this.props.dispatch(getArticleDataAsyncAction({ page: 1, pageSize: 10 }));
+    const { dispatch } = this.props;
+    const { page, pageSize, query } = this.state;
+    dispatch(getArticleDataAsyncAction({ page, pageSize, query }));
   }
+
   state = {
     isContentDialogOpen: false,
-    dialogData: null
+    dialogData: null,
+    page: 1,
+    pageSize: 10,
+    query: ""
   };
 
   parseArticle = article => event => {
@@ -44,11 +59,75 @@ class ArticlesPage extends Component {
     });
   };
 
+  handleQueryChange = event => {
+    const { name, value } = event.target;
+    this.setState(
+      { [name]: value },
+      value !== "" ? this.componentDidMount : () => {}
+    );
+  };
+
+  handleQueryStringChange = ({ target: { name, value } }) =>
+    this.setState({ [name]: value });
+
   render() {
     const { articleData, isMobile } = this.props;
-    const { isContentDialogOpen, dialogData } = this.state;
+    const {
+      isContentDialogOpen,
+      dialogData,
+      pageSize,
+      page,
+      query
+    } = this.state;
     return (
       <React.Fragment>
+        <FormGroup className="query-toolbar">
+          <div className="control">
+            <FormControl fullWidth variant="outlined">
+              <InputLabel>Page Size</InputLabel>
+              <Select
+                name="pageSize"
+                value={pageSize}
+                onChange={this.handleQueryChange}
+                input={<OutlinedInput />}
+              >
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={50}>Twenty</MenuItem>
+                <MenuItem value={100}>Thirty</MenuItem>
+              </Select>
+            </FormControl>
+          </div>
+          <div className="control">
+            <TextField
+              variant="outlined"
+              label="Page number"
+              value={page}
+              name="page"
+              onChange={this.handleQueryChange}
+              fullWidth
+            />
+          </div>
+          <div className="control">
+            <TextField
+              variant="outlined"
+              label="Query"
+              value={query}
+              name="query"
+              fullWidth
+              onChange={this.handleQueryStringChange}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment
+                    onClick={this.handleQueryChange}
+                    position="end"
+                  >
+                    <Search />
+                  </InputAdornment>
+                )
+              }}
+            />
+          </div>
+        </FormGroup>
         {articleData &&
           articleData.map((article, i) => (
             <ExpansionPanel key={i}>
