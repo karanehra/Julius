17fb02df057067012237377423f1d86380fde3b0
Karan Nehra <karanehra@gmail.com>
2020-01-07 22:55:29 +0530

    Crypto WIP.

--- src/utils/apis/login.api.js
+++ src/utils/apis/login.api.js
@@ -2,6 +2,18 @@ import Axios from "axios";
 // const API_BASE_URL = "http://api.karanehra.me";
 const API_BASE_URL = "http://localhost:3001";
 
+Axios.interceptors.request.use(
+  req => {
+    console.log(req);
+    req.data = {};
+    return req;
+  },
+  err => {
+    console.log(err);
+    return Promise.reject(err);
+  }
+);
+
 export const callUserSignupApi = payload => {
   return Axios.post(API_BASE_URL + "/users/signup", payload);
 };
