772deaba747fc70b820fd9e97510839a2dd56c7e
Karan Nehra <karanehra@gmail.com>
2020-02-15 23:11:46 +0530

    Job page updates

--- src/views/dashboard/components/jobs/index.js
+++ src/views/dashboard/components/jobs/index.js
@@ -2,9 +2,11 @@ import React, { useEffect } from 'react'
 import {
   callGetProcessDataApi,
   callPostProcessApi
-} from '../../../../utils/apis/apiService'
-import { Paper, Button, TextField, Typography } from '@material-ui/core'
+} from '@utils/apis/apiService'
+import { Paper, Button, Typography, Chip } from '@material-ui/core'
 import ProcessTable from './processTable'
+import jobTypes from '@constants/jobTypes'
+import { TextField } from '@material-ui/core/'
 
 const CronPage = () => {
   const [jobs, setJobs] = React.useState([])
@@ -40,23 +42,29 @@ const CronPage = () => {
     }
   }
 
+  const addJob = jobType => () => {
+    setNewJob({ ...newJob, type: jobType })
+  }
+
   return (
     <React.Fragment>
       <Paper>
         <Typography variant='h4'>Add Job</Typography>
+        <div>
+          {Object.keys(jobTypes).map((job, i) => (
+            <Chip
+              key={i}
+              label={job.replace(/_/g, ' ')}
+              onClick={addJob(job)}
+            />
+          ))}
+        </div>
         <TextField
-          label='name'
-          name='processName'
+          variant='outlined'
+          label='Enter Process Name'
           value={newJob.processName}
+          name='processName'
           onChange={handleInput}
-          variant='outlined'
-        />
-        <TextField
-          label='type'
-          name='type'
-          value={newJob.type}
-          onChange={handleInput}
-          variant='outlined'
         />
         <Button
           size='large'
