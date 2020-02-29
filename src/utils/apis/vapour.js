419d505b2f418d6d0f0a3abc0c7854aa6a3a02e6
Karan Nehra <karanehra@gmail.com>
2020-02-29 23:58:43 +0530

    More work on vapour panel

--- src/utils/apis/vapour.js
+++ src/utils/apis/vapour.js
@@ -8,3 +8,10 @@ const VAPOUR_BASE_URL = 'http://localhost:3009'
 export const callGetVapourStatusApi = () => {
   return Axios.get(`${VAPOUR_BASE_URL}/status`)
 }
+
+/**
+ * Retrieves the cache shard data and key data
+ */
+export const callGetVapourShardsApi = () => {
+  return Axios.get(`${VAPOUR_BASE_URL}/analytics/main`)
+}
--- src/views/dashboard/components/vapour/index.js
+++ src/views/dashboard/components/vapour/index.js
@@ -1,21 +1,35 @@
 import React, { useEffect } from 'react'
-import { callGetVapourStatusApi } from '../../../../utils/apis/vapour'
-import { Typography } from '@material-ui/core'
+import {
+  callGetVapourStatusApi,
+  callGetVapourShardsApi
+} from '../../../../utils/apis/vapour'
+import { Typography, Button } from '@material-ui/core'
 
 const Vapour = () => {
   const [isOnline, setIsOnline] = React.useState(false)
+  const [shards, setShards] = React.useState([])
 
   const getStatus = async () => {
     let res = await callGetVapourStatusApi()
-    console.log(res)
     setIsOnline(res.status === 200)
+    if (res.status === 200) {
+      let shardsResp = await callGetVapourShardsApi()
+      if (shardsResp.status === 200) {
+        setShards(shardsResp.data.shards)
+      }
+    }
   }
+
   useEffect(() => {
     getStatus()
   }, [])
+
   return (
     <>
+      <Typography variant='h4'>Vapour</Typography>
       <Typography>Status:{isOnline ? 'ONLINE' : 'OFFLINE'}</Typography>
+      <Button>Refresh</Button>
+      {shards.length && <div>Shard Data:</div>}
     </>
   )
 }
