690c60edc6c50133993e5fc443543c82bdccc3a1
Karan Nehra <karanehra@gmail.com>
2020-11-08 22:57:53 +0530

    Started to move to typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,48 @@
-const htmlWebPackPlugin = require("html-webpack-plugin");
-const cssPlugin = require("mini-css-extract-plugin");
-const path = require("path");
-const webpack = require("webpack");
+const htmlWebPackPlugin = require('html-webpack-plugin')
+const cssPlugin = require('mini-css-extract-plugin')
+const path = require('path')
+const webpack = require('webpack')
+
 module.exports = env => {
   return {
     module: {
       rules: [
         {
-          test: /\.js$/,
+          test: /\.tsx$/,
           exclude: /node_modules/,
-          use: ["babel-loader"]
+          use: ['babel-loader']
         },
         {
           test: /\.s?css$/,
           use: [
-            env.NODE_ENV === "production" ? cssPlugin.loader : "style-loader",
-            "css-loader",
-            "sass-loader"
+            env.NODE_ENV === 'production' ? cssPlugin.loader : 'style-loader',
+            'css-loader',
+            'sass-loader'
           ]
         },
         {
           test: /\.(png|gif|svg)$/,
-          use: ["file-loader"]
+          use: ['file-loader']
         }
       ]
     },
     output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "[hash].js"
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[hash].js'
     },
     optimization: {
       splitChunks: {
-        chunks: "all"
+        chunks: 'all'
       }
     },
     plugins: [
       new htmlWebPackPlugin({
-        template: "./src/index.html",
-        filename: "./index.html"
+        template: './src/index.html',
+        filename: './index.html'
       }),
       new webpack.DefinePlugin({}),
       new cssPlugin({
-        filename: "[name].css"
+        filename: '[name].css'
       })
     ],
     devServer: {
@@ -51,15 +52,15 @@ module.exports = env => {
     },
     resolve: {
       alias: {
-        "@actions": path.resolve(__dirname, "src/actions"),
-        "@assets": path.resolve(__dirname, "src/assets"),
-        "@constants": path.resolve(__dirname, "src/constants"),
-        "@shared": path.resolve(__dirname, "src/shared"),
-        "@styles": path.resolve(__dirname, "src/styles"),
-        "@utils": path.resolve(__dirname, "src/utils"),
-        "@views": path.resolve(__dirname, "src/views")
+        '@actions': path.resolve(__dirname, 'src/actions'),
+        '@assets': path.resolve(__dirname, 'src/assets'),
+        '@constants': path.resolve(__dirname, 'src/constants'),
+        '@shared': path.resolve(__dirname, 'src/shared'),
+        '@styles': path.resolve(__dirname, 'src/styles'),
+        '@utils': path.resolve(__dirname, 'src/utils'),
+        '@views': path.resolve(__dirname, 'src/views')
       },
-      extensions: [".js", ".scss"]
+      extensions: ['.tsx', '.js', '.scss']
     }
-  };
-};
+  }
+}
