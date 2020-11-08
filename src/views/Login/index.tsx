a4bc5d02b378de034e0abb3fb2ea695bdf914900
Karan Nehra <karanehra@gmail.com>
2020-11-08 23:59:37 +0530

    Added login form basic

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -1,7 +1,31 @@
-import * as React from 'react'
+import React, { useState } from 'react'
+import './index.scss'
+import { Paper, TextField, Button } from '@material-ui/core'
 
-const LoginView: React.SFC = () => {
-  return <div>Hello</div>
+const LoginView: React.FC = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const handleFormSubmission = () => {
+    console.log('submit')
+  }
+  return (
+    <Paper>
+      <form onSubmit={handleFormSubmission}>
+        <TextField
+          label='username'
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+        />
+        <TextField
+          label='password'
+          type='password'
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+        <Button type='submit'>Submit</Button>
+      </form>
+    </Paper>
+  )
 }
 
 export default LoginView
