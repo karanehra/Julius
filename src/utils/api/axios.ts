1637c07c80f560410caaa5c04304d113db2b5866
Karan Nehra <karanehra@gmail.com>
2020-11-21 23:11:17 +0530

    Completed the symmetric key encyption interceptors

--- src/utils/api/axios.ts
+++ src/utils/api/axios.ts
@@ -1,8 +1,9 @@
-import Axios, { AxiosRequestConfig } from 'axios'
+import Axios, { AxiosRequestConfig, AxiosResponse, AxiosError } from 'axios'
 import { LOGIN_PAGE_ROUTE } from '../../constants/routerUrls'
 import { AppStore } from '../../store'
 import history from '../history'
 import crypto from 'crypto'
+import { decryptIncomingResponse, encryptOutgoingRequest } from '@utils/crypto'
 
 const instance = Axios.create({ baseURL: 'http://localhost:9000' })
 
@@ -13,28 +14,19 @@ const outgoingRequestInterceptor = (request: AxiosRequestConfig) => {
       Authorization: `Bearer ${user.token}`
     }
   }
+  if (request.data) request = encryptOutgoingRequest(request)
   return request
 }
 
-const incomingResponseErrorInterceptor = error => {
-  const { status } = error.response
-  if (status === 401) {
+const incomingResponseErrorInterceptor = (error: AxiosError) => {
+  const res = decryptIncomingResponse(error.response)
+  if (res.status === 401) {
     history.push(LOGIN_PAGE_ROUTE)
   }
   return error
 }
 
-const incomingResponseInterceptor = response => {
-  let key = process.env.AES_KEY
-  let iv = response.data.iv
-  let decipher = crypto.createDecipheriv('aes-256-ctr', key, iv)
-  let decrypted = decipher.update(response.data.data, 'hex', 'utf8')
-  decrypted += decipher.final('utf8')
-  response.data = JSON.parse(decrypted)
-  return response
-}
-
 instance.interceptors.request.use(outgoingRequestInterceptor, error => error)
-instance.interceptors.response.use(incomingResponseInterceptor, incomingResponseErrorInterceptor)
+instance.interceptors.response.use(decryptIncomingResponse, incomingResponseErrorInterceptor)
 
 export default instance
---
+++ src/utils/crypto.ts
@@ -0,0 +1,25 @@
+import crypto from 'crypto'
+import { AxiosRequestConfig, AxiosResponse } from 'axios'
+
+export const decryptIncomingResponse = (response: AxiosResponse) => {
+  let key = process.env.AES_KEY
+  let iv = response.data.iv
+  let decipher = crypto.createDecipheriv('aes-256-ctr', key, iv)
+  let decrypted = decipher.update(response.data.data, 'hex', 'utf8')
+  decrypted += decipher.final('utf8')
+  response.data = JSON.parse(decrypted)
+  return response
+}
+
+export const encryptOutgoingRequest = (request: AxiosRequestConfig) => {
+  let key = process.env.AES_KEY
+  let iv = crypto.randomBytes(8).toString('hex')
+  let cipher = crypto.createCipheriv('aes-256-ctr', key, iv)
+  let encrypted = cipher.update(JSON.stringify(request.data), 'utf8', 'hex')
+  encrypted += cipher.final('hex')
+  request.data = {
+    data: encrypted,
+    iv
+  }
+  return request
+}
